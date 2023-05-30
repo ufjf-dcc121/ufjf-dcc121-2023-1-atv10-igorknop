90aa0fb5d93f687f913a015c5612e2faccf251d8
Igor Knop <igor.knop@ufjf.br>
2023-05-30 20:40:37 -0300

    isola o estado

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import store from "./store.js";
+import { adicionar, getItens } from "./store.js";
 
 const form = document.forms.entrada;
 form.addEventListener('submit', envia);
@@ -10,7 +10,7 @@ function envia(evento) {
     evento.preventDefault();
     console.log('Formulário enviado!');
     const n = form.valor.value;
-    store.estado.push(n);
+    adicionar(n);
     form.valor.value = "";
     form.valor.focus();
     atualiza();
@@ -19,9 +19,11 @@ function envia(evento) {
 function atualiza() {
     const ol = document.querySelector('ol');
     ol.innerHTML = "";
-    for(let i = 0; i < store.estado.length; i++){
+    const itens = getItens();
+    for (let i = 0; i < itens.length; i++) {
         const li = document.createElement('li');
-        li.textContent = store.estado[i];
+        li.textContent = itens[i];
         ol.appendChild(li);
     }
+    itens.push("Boom!");
 }
--- store.js
+++ store.js
@@ -1,2 +1,10 @@
-const store = { estado: [0,1,2,3] };
-export default store; 
+const store = { estado: [0, 1, 2, 3] };
+
+
+export function adicionar(valor) {
+    store.estado.push(valor);
+}
+
+export function getItens() {
+    return [...store.estado];
+}
